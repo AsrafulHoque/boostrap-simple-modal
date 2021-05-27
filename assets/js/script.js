187903d522626f1877885b448dc8cb8828a682f7
Asraful Hoque <67984098+AsrafulHoque@users.noreply.github.com>
2021-05-27 22:37:41 +0600

    Add files via upload
    first modal

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+$(document).ready(function(){
+    
+})
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
